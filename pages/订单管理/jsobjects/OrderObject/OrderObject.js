d3f1f05af4c1f63be6ce2ea4c11598fa774ed053
mangoguang <542436458@qq.com>
2024-04-24 05:15:37 +0000

    System generated commit, initial commit

---
+++ pages/订单管理/jsobjects/OrderObject/OrderObject.js
@@ -0,0 +1,23 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	addOrder: async () => {
+		// write code here
+		let res = await add_order.run(add_order_form.formData)
+		closeModal("add_order_form_modal")
+		showAlert("订单新增成功")
+		get_orders.run()
+	},
+	deleteOrder: async (params) => {
+		//use async-await or promises
+		try{
+				await delete_order.run(params)
+				console.log('params:123', params)
+				showAlert("订单删除成功")
+				closeModal("delete_order_modal")
+				get_orders.run()
+    }catch(error){
+				showAlert("订单删除失败")
+    }
+	}
+}
